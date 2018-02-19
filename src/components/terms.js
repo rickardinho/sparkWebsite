3a35eef756b6ecbb875de30de11cbb6425328dfc
rickardinho <d.rickardinho@gmail.com>
2018-02-19 13:15:54 +0000

    Create terms.js

---
+++ src/components/terms.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Terms = () => (
+  <div>
+    <div className="App">
+      <div className="App-header">
+        <h2>Terms</h2>
+      </div>
+      <p className="Termsstuff">
+        blah blah.
+      </p>
+    </div>
+  </div>
+);
+
+export default Terms;
