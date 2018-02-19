ec2d32953b3ca36e67039960f798555da4399775
rickardinho <d.rickardinho@gmail.com>
2018-02-19 13:14:53 +0000

    Create about.js

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const About = () => (
+  <div>
+    <div className="App">
+      <div className="App-header">
+        <h2>About</h2>
+      </div>
+      
+    </div>
+  </div>
+);
+
+export default About;
