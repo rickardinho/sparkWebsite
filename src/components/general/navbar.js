28be28e5e817d3a93b4a94800ffb8f0fc1db015b
rickardinho <d.rickardinho@gmail.com>
2018-02-19 17:34:39 +0000

    Navbarsetup

--- src/components/general/navbar.js
+++ src/components/general/navbar.js
@@ -5,35 +5,39 @@ import classnames from 'classnames';
 const Navbar = ({ currentLocation }) => {
 
     if (process.env.DEVELOPMENT) {
-        currentLocation = '/farmSummary';
+        currentLocation = '/about';
     }
 
     let navbarClasses = classnames("navbar", {
         "display-none": currentLocation === "/"
     });
 
-    let farmSummaryClasses = classnames("nav-button", {
-        "selected": currentLocation.indexOf("/farmSummary") > -1
+    let aboutClasses = classnames("nav-button", {
+        "selected": currentLocation.indexOf("/about") > -1
     });
 
-    let farmDetailClasses = classnames("nav-button", {
-        "selected": currentLocation.indexOf("/farmDetail") > -1
+    let helpClasses = classnames("nav-button", {
+        "selected": currentLocation.indexOf("/help") > -1
+    });
+    
+    let comtactClasses = classnames("nav-button", {
+        "selected": currentLocation.indexOf("/contact") > -1
     });
 
 
     return (
         <div className={ navbarClasses }>
-            <Link className={ farmSummaryClasses } to="farmSummary">
+            <Link className={ homeClasses } to="home">
                 <div >
                     <i className="fa fa-camera nav-icon "></i>
-                    <label className="menu-label">Farm Summary</label>
+                    <label className="menu-label">Home</label>
                 </div>
             </Link>
 
-            <Link className={ farmDetailClasses } to="farmDetail">
+            <Link className={ aboutClasses } to="about">
                 <div>
                     <i className="fa fa-calendar nav-icon"></i>
-                    <label className="menu-label">Farm Details</label>
+                    <label className="menu-label">About</label>
                 </div>
             </Link>
 
